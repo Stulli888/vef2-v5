446da2634d49e56edd821a1f583db1fbd94d32a4
Sturla <sfm1@hi.is>
2022-04-08 18:25:25 +0000

    users nær að tengja við server en fær invalid user/pw

--- pages/users/login.tsx
+++ pages/users/login.tsx
@@ -3,12 +3,38 @@ import Router from 'next/router'
 import { useUser } from '../../lib/hooks'
 
 function login(){
-		return (
-			<main>
-			<p>fff</p>
-			
-			</main>
-		)
+
+	const loginUser = async event => {
+    	event.preventDefault() // don't redirect the page
+    	const res = await fetch(
+      'https://vef2-20222-v3-synilausn.herokuapp.com/users/login',
+      		{
+      		  body: JSON.stringify({
+      		    name: event.target.name.value,
+      		    password: event.target.pw.value
+      		  }),
+      		  headers: {
+      		    'Content-Type': 'application/json'
+      		  },
+      		  method: 'POST'
+      		}
+    	)
+    	console.log('name: ', event.target.name.value)
+    	console.log('pw: ', event.target.pw.value)
+    	const result = await res.json()
+    	console.log(result)
+    }
+	return (
+		<main>
+		  <form onSubmit={loginUser}>
+       		<label htmlFor="name">username</label>
+       		<input id="name" name="name" type="text" autoComplete="name" required />
+       		<label htmlFor="pw">password</label>
+       		<input id="pw" name="pw" type="password" required />
+       		<button type="submit">Login</button>
+    	  </form>
+		</main>
+	)
 }
 
 export default login
